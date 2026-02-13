2aa6778b7fbddca6b3458ec6f1f14656b35106a6
Armin Hupka <arminhupka@me.com>
2026-02-13 17:52:29 +0100

    refactor: update apiClient to use environment variable for base URL

--- app/lib/apiClient.ts
+++ app/lib/apiClient.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const apiClient = axios.create({ baseURL: "http://localhost:8888" });
+export const apiClient = axios.create({
+	baseURL: import.meta.env.VITE_API_URL,
+});
 
 apiClient.interceptors.request.use((config) => {
 	const token = localStorage.getItem("token");
